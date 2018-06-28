7bdfd04a9de9f3e4ab52a9e0afea1c17a093ecd5
AtomikKeo <kekoka3m0t@gmail.com>
2018-06-28 20:33:00 +0900

    add comment

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
+//ミドルウェアの使用
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json())
 
