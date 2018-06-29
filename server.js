ad2908bc97d54c3b53ae7f0a7a7355e5cef93a9e
AtomikKeo <kekoka3m0t@gmail.com>
2018-06-29 22:57:48 +0900

    create client side

--- server.js
+++ server.js
@@ -6,8 +6,49 @@ const bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.json())
 
+mongoose.connect('mongodb://localhost/mydb');
+
+const Todo = mongoose.model('Todo', {
+  text : String
+});
+
+app.get('/api/todos', (req, res) =>{
+  Todo.find()
+      .then((todos) => {
+        res.json(todos);
+      })
+      .catch((err) => {
+        res.send(err);
+      })
+});
+
+app.post('/api/todos', (req, res) => {
+  const todo = req.body;
+  Todo.create({
+        text : todo.text,
+      })
+      .then((todo) => {
+        res.json(todo);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+});
+
+app.delete('/api/todos/:todo_id', (req, res) => {
+  Todo.remove({
+        _id : req.params.todo_id
+      })
+      .then((todo) => {
+        res.send(todo);
+      })
+      .catch((err) => {
+        res.send(err);
+      });
+});
+
 app.get('/', (req, res) => {
-  res.send('Hello Node.js!');
+  res.sendfile('/public/index.html');
 });
 
 app.listen(3000, () => {
